9245caaa877cb77ac8359303c1551f17b0819012
Coderkhayrul <coderkhayrul>
2021-08-28 05:18:40 +0600

    Company Page Desing

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -2,42 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header bg-primary m-0 text-white d-flex justify-content-between">
-                    {{Str::limit($company->title, 90)}}
-                    <a href="{{ url('/') }}" class="btn btn-success btn-sm">Back</a>
-                </div>
-
-                <div class="card-body">
-                    <p>
-                        <h3>Description</h3>
-                        {{ $company->description }}
-                    </p>
-                    <p>
-                        <h3>Duties</h3>
-                        {{ $company->roles }}
-                    </p>
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="company-profile">
+                <img src="{{ asset('cover/php_cover.jpg') }}" alt="Company Cover Image" style="width:100%;">
+                <div class="company-description">
+                    <img style="width:100px" src="{{ asset('avatar/logo.png') }}" alt="Company Logo">
+                    {{ $company->description }}
+                    <h1>{{ $company->cname }}</h1>
+                    <p><strong>Slogan</strong> - {{ $company->slogan }} &nbsp;<strong>Address</strong> - {{ $company->address }} &nbsp; <strong>Phone</strong> - {{ $company->phone }} &nbsp; <strong>Website</strong> - {{ $company->website}}</p>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header bg-success text-white">Short Info</div>
-
-                <div class="card-body">
-                    <p>Company: <a style="text-decoration: none" href="#">{{ $company->cname }}</a></p>
-                    <p>Address: {{ $company->address }}</p>
-                    <p>Employee Type: {{ $company->type }}</p>
-                    <p>Position: {{ $company->position }}</p>
-                    <p>Date: {{ $company->created_at->diffForHumans() }}</p>
-                </div>
-            </div>
-            <br>
-            @if (Auth::check() && Auth::user()->user_type === 'seeker')
-            <a style="width:100%" href="" class="btn btn-success">Apply</a>
-            @endif
         </div>
     </div>
 </div>
