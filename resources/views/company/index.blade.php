808542509013164f196f5fd7169b9a713c5f22bb
Coderkhayrul <coderkhayrul>
2021-08-28 06:23:32 +0600

    Post Show By Company

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -13,6 +13,29 @@
                     <p><strong>Slogan</strong> - {{ $company->slogan }} &nbsp;<strong>Address</strong> - {{ $company->address }} &nbsp; <strong>Phone</strong> - {{ $company->phone }} &nbsp; <strong>Website</strong> - {{ $company->website}}</p>
                 </div>
             </div>
+            <br>
+            <table class="table table-hover">
+                <thead>
+                    <th>Logo</th>
+                    <th>Position</th>
+                    <th>Address</th>
+                    <th>Date</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    @foreach ($company->jobs as $job)
+                        <tr>
+                            <td><img width="80" src="{{ asset('avatar/logo.png') }}" alt=""></td>
+                            <td>Position: {{ $job->position }} <br> <i class="fas fa-business-time text-primary"></i> {{ $job->type }}</td>
+                            <td><i class="fas fa-map-marker-alt text-primary"></i>&nbsp;Address: {{ $job->address }}</td>
+                            <td><i class="fas fa-globe-asia text-primary"></i>&nbsp;Date: {{ $job->created_at->diffForHumans() }}</td>
+                            <td>
+                                <a class="btn btn-success btn-sm"href="{{ route('jobs.show',[$job->id,$job->slug]) }}">View</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
